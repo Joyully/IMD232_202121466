a79e8e53fae6d74e1f0f8d113ea436eb010e2755
Joyully <143991481+Joyully@users.noreplay.github.com>
2023-10-16 21:19:21 +0900

    w5 assignment

--- src/w5/assignment/step1/script/sketch.js
+++ src/w5/assignment/step1/script/sketch.js
@@ -3,16 +3,14 @@ const rNum = 8;
 const ellipseBegin = 70;
 const ellipseGap = 39;
 
-let gridC;
-let gridR;
 let angleBegin = 0;
+let angleStep = 1;
 let angleBeginVel;
-let angleStep;
+let angleIncrement = 15;
 
 function setup() {
   setCanvasContainer('myCanvas', 1, 1, true);
-
-  angleBeginVel = (TAU / 360) * 1;
+  angleBeginVel = (TAU / 360) * angleStep;
   background('gainsboro');
 }
 
@@ -22,7 +20,20 @@ function draw() {
 
   for (let r = 0; r < rNum; r++) {
     for (let c = 0; c < cNum; c++) {
-      stroke((255 / rNum) * r, (255 / cNum) * c, 255);
+      if (r % 2 === 1) {
+        if (c % 2 === 1) {
+          stroke(0, 0, 255);
+        } else {
+          stroke(0, 255, 0);
+        }
+      } else {
+        if (c % 2 === 1) {
+          stroke(255, 255, 0);
+        } else {
+          stroke(255, 0, 0);
+        }
+      }
+
       let x = ellipseBegin + c * (3 * ellipseGap);
       let y = ellipseBegin + r * (3 * ellipseGap);
 
@@ -30,15 +41,21 @@ function draw() {
 
       push();
       translate(x, y);
-      rotate(angleBegin);
+
+      let currentAngle =
+        angleBegin + (TAU / 360) * ((c + r * cNum) * angleIncrement);
+      rotate(currentAngle);
+
       line(0, 0, 39, 0);
       pop();
 
-      // Draw rotating circle at the end of each line
-      let circleX = x + 39 * cos(angleBegin);
-      let circleY = y + 39 * sin(angleBegin);
+      fill('deeppink');
+      let circleX = x + 39 * cos(currentAngle);
+      let circleY = y + 39 * sin(currentAngle);
       ellipse(circleX, circleY, 10);
+
+      noFill();
     }
   }
-  angleBegin += angleBeginVel;
+  angleBegin += (TAU / 360) * angleStep;
 }
